ff9f2140e750236f274c330d9be7c630f40acb6d
marek <marek.wierzchowski91@gmail.com>
2021-03-31 18:58:35 +0200

    My third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Napis testowy");
         System.out.println("Drugi napis testowy");
+        System.out.println("Trzeci napis");
     }
 }
